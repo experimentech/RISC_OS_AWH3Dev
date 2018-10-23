0e4e2f5ea604a835138cf56b510e9e634ca20cc9
riscos <riscos@cd4849fb-8707-413f-82c4-fc0884dbda23>
2018-10-23 02:47:48 +0000

    DDE doesn't support 0b format for binary numbers.

--- RISC_OS_Dev/castle/RiscOS/Sources/Video/HWSupport/NewVideo/misc.h
+++ RISC_OS_Dev/castle/RiscOS/Sources/Video/HWSupport/NewVideo/misc.h
@@ -1,12 +1,12 @@
 #ifndef MISC_H
 #define MISC_H
 
-#define MODE_1BPP    0b000001
-#define MODE_2BPP    0b000010
-#define MODE_4BPP    0b000100
-#define MODE_8BPP    0b001000
-#define MODE_16BPP   0b010000
-#define MODE_32BPP   0b100000
+#define MODE_1BPP    0x1  //0b000001
+#define MODE_2BPP    0x2  //0b000010
+#define MODE_4BPP    0x4  //0b000100
+#define MODE_8BPP    0x8  //0b001000
+#define MODE_16BPP   0x10 //0b010000
+#define MODE_32BPP   0x20 //0b100000
 
 
 #endif
